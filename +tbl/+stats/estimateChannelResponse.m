cf03a7b780822d858d517b49790d606c4dbd5440
Max-Home-Tower <max.murphy11@gmail.com>
2020-10-02 11:45:34 -0500

    Update estimateChannelResponse

--- +tbl/+stats/estimateChannelResponse.m
+++ +tbl/+stats/estimateChannelResponse.m
@@ -59,11 +59,15 @@ if ~ismember('ElectrodeID',T.Properties.VariableNames)
    T.ElectrodeID = strcat(string(T.SurgID),"-",string(T.ChannelID));
 end
 
+if ~ismember('StimLamina',T.Properties.VariableNames)
+   T = tbl.addStimLamina(T);
+end
+
 if strcmp(T.Properties.UserData.type,'MasterTable')
-   [G,C] = findgroups(T(:,{'GroupID','SurgID','AnimalID','ElectrodeID','BlockID','BlockIndex','Type','Area','ChannelID','AP','ML','Depth','Channel','Stim_Ch','ICMS_Onset','Solenoid_Onset','Solenoid_Offset','Impedance','coeff','p'}));
+   [G,C] = findgroups(T(:,{'GroupID','SurgID','AnimalID','ElectrodeID','BlockID','BlockIndex','Type','Area','ChannelID','AP','ML','Depth','Channel','Stim_Ch','StimLamina','StimDepth','ICMS_Onset','Solenoid_Onset','Solenoid_Offset','Impedance','coeff','p'}));
    C.Properties.UserData.type = 'ChannelResponseTable';
 else
-   G = findgroups(T(:,{'GroupID','SurgID','AnimalID','ElectrodeID','BlockID','BlockIndex','Type','Area','ChannelID','AP','ML','Depth','Channel','Stim_Ch','ICMS_Onset','Solenoid_Onset','Solenoid_Offset','Impedance','coeff','p'}));
+   G = findgroups(T(:,{'GroupID','SurgID','AnimalID','ElectrodeID','BlockID','BlockIndex','Type','Area','ChannelID','AP','ML','Depth','Channel','Stim_Ch','StimLamina','StimDepth','ICMS_Onset','Solenoid_Onset','Solenoid_Offset','Impedance','coeff','p'}));
    C = T;
 end
 tmp = splitapply(fcn,T(:,inputVars),G);
